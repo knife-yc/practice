98408ffa5a0405d5340822f7fe5cb770d5c72c9c
pocket_knife <pocket_knife@163.com>
2020-05-21 15:19:25 +0800

    1111

--- practice-test/src/main/java/com/yc/practice/test/cglib/ClassA.java
+++ practice-test/src/main/java/com/yc/practice/test/cglib/ClassA.java
@@ -3,4 +3,6 @@ package com.yc.practice.test.cglib;
 public interface ClassA {
 
     void callMethod();
+
+    void test(String a);
 }
--- practice-test/src/main/java/com/yc/practice/test/cglib/ClassProxyTest.java
+++ practice-test/src/main/java/com/yc/practice/test/cglib/ClassProxyTest.java
@@ -7,5 +7,7 @@ public class ClassProxyTest {
         //单步调试时IDEA会调用被代理类的toString()方法，代理ClassA类会代理该类的所有方法（包括toString和hashCode），因此会重复输出
         ClassA a = ClassProxy.getProxyInstance(ClassA.class,"test","1.0.0");
         a.callMethod();
+        //接口调用时会通过代理的intercept方法触发
+        a.test("test");
     }
 }
